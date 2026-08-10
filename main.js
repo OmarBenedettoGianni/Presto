a8b2a87fe3479a9424428ac6993bfa5c9d499cb1
OmarBenedettoGianni <omarbenedetto200012@gmail.com>
2026-08-10 14:51:16 +0200

    sezione postioned

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ window.addEventListener('scroll',()=>{
     let scrolled =window.scrollY;
 
     if(scrolled > 0){
-        navbar.classList.remove('bg-black');
+        navbar.classList.remove('bg-yellow');
         navbar.classList.add('bg-red');
         navbar.style.height= '140px';
         links.forEach((link)=>{
@@ -14,12 +14,12 @@ window.addEventListener('scroll',()=>{
         });
         logoNavbar.src='http://127.0.0.1:5500/Media/logoWhite512px.png';
     }else{
-        navbar.classList.add('bg-black');
+        navbar.classList.add('bg-yellow');
         navbar.classList.remove('bg-red');
         navbar.style.height= '70px';
         links.forEach((link)=>{
-            link.style.color= 'var(--yellow)'
+            link.style.color= 'var(--white)'
         });
-       logoNavbar.src='http://127.0.0.1:5500/Media/logoYellow512px.png'
+       logoNavbar.src='http://127.0.0.1:5500/Media/logoBlack512px.png'
     }
 })
