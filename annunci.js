84d7196ba3ddfaf791ddbe0cd3d6a3d2a50ddaed
OmarBenedettoGianni <omarbenedetto200012@gmail.com>
2026-08-11 18:41:57 +0200

    inizializzato filtro categoria

--- annunci.js
+++ annunci.js
@@ -1,3 +1,26 @@
 fetch('./annunci.json').then((response)=>response.json()).then((data)=>{
-    
+   let radioWrapper = document.querySelector('#radioWrapper');
+   function radioCreate(){
+        let categories = data.map((annuncio)=>annuncio.category);
+        // let uniqueCategories = [];
+        // categories.forEach((category) => {
+        //     if(!uniqueCategories.includes(category)){
+        //         uniqueCategories.push(category)
+        //     }
+        // });
+        let uniqueCategories = Array.from(new Set(categories));
+        uniqueCategories.forEach((category)=>{
+            let div = document.createElement('div');
+            div.classList.add('form-check');
+            div.innerHTML = `
+                <input class="form-check-input" type="radio" name="categories" id="${category}">
+                    <label class="form-check-label" for="${category}">
+                        ${category}
+                    </label>
+            `;
+            radioWrapper.appendChild(div);
+
+        });
+   }
+   radioCreate();
 })
