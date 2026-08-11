fe3f67e4bffbf97fb8d0a8d0af415dc7c0eb811b
OmarBenedettoGianni <omarbenedetto200012@gmail.com>
2026-08-11 10:54:34 +0200

    creati numeri incrementali

--- main.js
+++ main.js
@@ -1,13 +1,20 @@
 let navbar = document.querySelector('#navbar');
 let links = document.querySelectorAll('.nav-link');
 let logoNavbar = document.querySelector('#logoNavbar');
-console.dir(logoNavbar);
+let collapse = document.querySelector('#collapse');
+let firstNumber = document.querySelector('#firstNumber');
+let secondNumer = document.querySelector('#secondNumber');
+let thirdNumber = document.querySelector('#thirdNumber');
+let confirm = true
+
+
 window.addEventListener('scroll',()=>{
     let scrolled =window.scrollY;
-
     if(scrolled > 0){
         navbar.classList.remove('bg-yellow');
         navbar.classList.add('bg-red');
+        collapse.classList.remove('bg-yellow');
+        collapse.classList.add('bg-red');
         navbar.style.height= '140px';
         links.forEach((link)=>{
             link.style.color= 'var(--white)'
@@ -16,6 +23,8 @@ window.addEventListener('scroll',()=>{
     }else{
         navbar.classList.add('bg-yellow');
         navbar.classList.remove('bg-red');
+        collapse.classList.add('bg-yellow');
+        collapse.classList.remove('bg-red');
         navbar.style.height= '70px';
         links.forEach((link)=>{
             link.style.color= 'var(--white)'
@@ -23,3 +32,31 @@ window.addEventListener('scroll',()=>{
        logoNavbar.src='http://127.0.0.1:5500/Media/logoBlack512px.png'
     }
 })
+
+function createInterval (n, element, time){
+    let counter = 0;
+    let interval = setInterval(()=>{
+        if(counter < n){
+            counter ++;
+            element.innerHTML = counter;
+        }else{
+            clearInterval(interval);
+        }
+    },time);
+    setTimeout(()=>{
+        confirm=true
+    },13000);
+}
+
+let observer = new IntersectionObserver((entries)=>{
+    entries.forEach((entry)=>{
+        if(entry.isIntersecting && confirm){
+            createInterval(100,firstNumber,100);
+            createInterval(500,secondNumer,20);
+            createInterval(50,thirdNumber,200);
+            confirm = false;
+        }
+    });
+});
+
+observer.observe(firstNumber);
