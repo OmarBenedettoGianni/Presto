aba1f56653ac264e9b960a810bec704d7f4100f0
OmarBenedettoGianni <omarbenedetto200012@gmail.com>
2026-08-11 12:33:23 +0200

    prima pag terminata,fare footer

--- main.js
+++ main.js
@@ -11,25 +11,25 @@ let confirm = true
 window.addEventListener('scroll',()=>{
     let scrolled =window.scrollY;
     if(scrolled > 0){
-        navbar.classList.remove('bg-yellow');
-        navbar.classList.add('bg-red');
-        collapse.classList.remove('bg-yellow');
-        collapse.classList.add('bg-red');
+        // navbar.classList.remove('bg-yellow');
+        // navbar.classList.add('bg-red');
+        // collapse.classList.remove('bg-yellow');
+        collapse.classList.add('bg-yellow');
         navbar.style.height= '140px';
         links.forEach((link)=>{
             link.style.color= 'var(--white)'
         });
-        logoNavbar.src='http://127.0.0.1:5500/Media/logoWhite512px.png';
+        //logoNavbar.src='http://127.0.0.1:5500/Media/logoWhite512px.png';
     }else{
-        navbar.classList.add('bg-yellow');
-        navbar.classList.remove('bg-red');
-        collapse.classList.add('bg-yellow');
-        collapse.classList.remove('bg-red');
+        // navbar.classList.add('bg-yellow');
+        // navbar.classList.remove('bg-red');
+        // collapse.classList.add('bg-yellow');
+        // collapse.classList.remove('bg-red');
         navbar.style.height= '70px';
         links.forEach((link)=>{
             link.style.color= 'var(--white)'
         });
-       logoNavbar.src='http://127.0.0.1:5500/Media/logoBlack512px.png'
+       //logoNavbar.src='http://127.0.0.1:5500/Media/logoBlack512px.png'
     }
 })
 
@@ -60,3 +60,61 @@ let observer = new IntersectionObserver((entries)=>{
 });
 
 observer.observe(firstNumber);
+
+
+let reviews = [
+    {user:'Altaïr', description:`Sito bellissimo e utilissimo`,rank: 5},
+    {user:'Connor', description:`Si potrebbe fare di meglio`,rank: 3},
+    {user:'Edward', description:`Pubblicità ingannevole, se potessi darei 0 stelle`,rank: 1},
+    {user:'Yasuke', description:`L'importante è che funzioni`,rank: 4},
+]
+
+let swiperWrapper = document.querySelector('.swiper-wrapper')
+reviews.forEach((recensione)=>{
+    let div = document.createElement('div');
+    div.classList.add('swiper-slide');
+    div.innerHTML=`
+        <div class="card-review">
+            <p class="lead text-center">${recensione.description}</p>
+            <p class="h-4 text-center">${recensione.user}</p>
+            <div class="d-flex justify-content-center star">        
+            </div>  
+        </div>
+    `;
+    swiperWrapper.appendChild(div);
+});
+
+// piena  <i class="fa-solid fa-star"></i>
+// vuota  <i class="fa-regular fa-star"></i>
+
+let stars = document.querySelectorAll('.star')
+stars.forEach((star,index)=>{
+    for(let i = 1; i <= reviews[index].rank; i++ ){
+        let icon = document.createElement('i');
+        icon.classList.add('fa-solid', 'fa-star');
+        star.appendChild(icon);
+    }
+    let difference = 5 - reviews[index].rank
+    for(let i = 1; i <= difference; i++ ){
+        let icon = document.createElement('i');
+        icon.classList.add('fa-regular', 'fa-star');
+        star.appendChild(icon);
+    }
+
+});
+
+// Swiper JS
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+    effect: 'flip',
+    grabCursor: true,  
+    loop: true,
+  // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    autoplay : {
+        delay: 5000,
+    },
+});
